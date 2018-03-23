5c12c3bf183ade5ab68463ba1fa580bfd484a18f
bigcat <1025724457@qq.com>
2018-03-23 20:39:46 +0800

    修改db

--- FangyuanHelper/FangyuanHelper/settings.py
+++ FangyuanHelper/FangyuanHelper/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'fangyuan',
+    'fangyuan.apps.FangyuanConfig',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'FangyuanHelper.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fangyuan_helper',
+        'USER': 'root',
+        'PASSWORD': 'sa',
+        'HOST': 'localhost',
+        'PORT': '3306',
+
     }
 }
 
--- FangyuanHelper/fangyuan/models.py
+++ FangyuanHelper/fangyuan/models.py
@@ -1,3 +1,15 @@
+# -*- coding: UTF-8 -*-
 from django.db import models
 
-# Create your models here.
+
+class HouseInfo(models.Model):
+    title = models.CharField(max_length=100)  # 房源标题
+    url = models.CharField(max_length=500)  # 房源连接
+    price = models.CharField(max_length=10)  # 房源价格
+    zuping = models.CharField(max_length=20)  # 房源租凭（整租/合租）
+    size = models.CharField(max_length=20)  # 房源大小
+    xiaoqu = models.CharField(max_length=20)  # 房源所在小区
+    area = models.CharField(max_length=20)  # 房源所在地区
+    detailed_address = models.CharField(max_length=60)  # 房源详细地址
+    # phone = models.CharField(max_length=20)  # 房东电话
+    host = models.CharField(max_length=20)  # 房源联系人
